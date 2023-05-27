e914a2a4ac531853faf03428de9ed625202ad4e6
Mateus Gutemberg <mateus.gut432@gmail.com>
2023-05-27 18:37:08 -0300

    begining of the function server() and dispatcher_thread_function() created

--- main.c
+++ main.c
@@ -3,23 +3,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define N 10; //constant of the program that indicate the number of
+ #define N_WTHREADS  10 //constant of the program that indicate the number of
 //workers threads
 
+/**
+ * @brief 
+ * 
+ */
 struct requests_{
     int digit_pi;
     int time_waiting;
 }requests;
 
+/**
+ * @brief 
+ * 
+ * @param fp 
+ */
+void* dispatcher_thread_function(void *fp){
+    pthread_t threads[N_WTHREADS];
+    FILE *requests_file = fp;
+    int digits, time_waiting;
+    if(requests_file != NULL){
+        while(fscanf(fp, "%d;%d", &digits, &time_waiting) != EOF){
+            printf("%d %d\n", digits, time_waiting);    
+        }
+    }else{
+        printf("ERROR IN OPEN THE FILE!");
+    }
+    pthread_exit(NULL);
+}
+
 
 /**
  * * @brief this function will receive the requests, instantiate the worker threads and
  * the dispatcher thread to help the server to process the requests of the clients.
- * @param fp //pinter to the archive
+ * @param fp //pointer to the archive
  * @param n_threads //number of worker threads
  */
 void server(FILE *fp){
-    pthread_create();
+    int r = 0;
+    pthread_t dispatcher;
+    r = pthread_create(&dispatcher, NULL, dispatcher_thread_function, (void*)fp);
+    if(r){
+        printf("ERROR IN THE CREATION OF THE THREAD!\n");//if r is different than 0
+        exit(-1);
+    }else{
+        printf("DISPATCHER THREAD CREATED WITH SUCCESS!\n");//if r is equal to 0
+    }
+    pthread_join(dispatcher, NULL);
 }
 
 /**
@@ -27,28 +59,24 @@ void server(FILE *fp){
  * requests for the server
  * @return int 
  */
+//inicio: 0x7ff8a890fa90
 int main(){
     srand(time(0));
     FILE *fp = fopen("requests.txt", "w");
-    int num = 0;
     if(fp != NULL){
         int digits = 0, time_waiting = 0;
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 100; i++){
             digits = (rand() % (100 - 10 + 1)) + 10;//PI's numbers of digits 
             time_waiting = (rand() % (1500 - 500 + 1)) + 500;//time of waiting
             fprintf(fp, "%d;%d\n", digits, time_waiting);
         }
+
         fclose(fp);
         fp = fopen("requests.txt", "r");
-        if(fp != NULL){
-            FILE *fp2 = fopen("requests2.txt", "w");
-            while(fscanf(fp, "%d;%d", &digits, &time_waiting) != EOF){
-                fprintf(fp2, "%d %d\n", digits, time_waiting);    
-            }
-        }else{
+        if(fp != NULL)
+            server(fp);//call the server function
+        else
             printf("ERROR IN OPEN THE FILE!");
-        }
-        fclose(fp);
     }else{
         printf("ERROR IN OPEN THE FILE!");
     }
